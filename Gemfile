bb0028d2e644791a6c2d3fc75e110b4e97d6a578
tomohiro.zoda <tomohiro.zoda@dena.com>
2016-01-31 21:10:21 +0900

    twitterに送りつける機構を実装

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'kindle-highlights'
+gem 'twitter'
+gem 'dotenv'
+
+group :development do
+  gem 'pry'
+  gem 'pry-byebug'
+end
---
+++ lib/kindle.rb
@@ -0,0 +1,24 @@
+require 'kindle_highlights'
+
+class JpKindleHighlights < KindleHighlights::Client
+  KindleHighlights::KINDLE_LOGIN_PAGE = 'http://kindle.amazon.co.jp/login'
+
+  def highlights_for(asin)
+    sleep 0.3
+
+    page = @mechanize_agent.get("https://kindle.amazon.co.jp/your_highlights_and_notes/#{asin}")
+
+    highlights = page.xpath('//div[@class="highlightRow yourHighlight"]').map do |e|
+      {
+        text: e.xpath('span[@class="highlight"]')&.inner_text || '',
+        location_href: e.css('a')&.attribute('href')&.value   || ''
+      }
+    end
+
+    {
+      book_title:  page.search('span.title').text.gsub(/\(Japanese Edition\)/, ''),
+      book_author: page.search('span.author').text.gsub(/\A by /, '').strip,
+      highlights:  highlights
+    }
+  end
+end
